dc9061a5f6e1c8d48075799f7a17b7bbc5460a4c
Blaine Bublitz <blaine.bublitz@gmail.com>
2022-03-16 14:55:35 -0700

    feat: Change the output path to client/public

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,6 +7,7 @@ module.exports = {
   solidity: "0.6.7",
   circom: {
     inputBasePath: "./circuits",
+    outputBasePath: "./client/public",
     ptau: "pot15_final.ptau",
     circuits: [
       {
